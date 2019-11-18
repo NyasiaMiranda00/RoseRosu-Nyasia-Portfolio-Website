c6fd9cec5b11f9b9f92d7270bbf4edcc17c1dd77
NyasiaMiranda00 <raven10ny@gmail.com>
2019-11-18 15:45:58 -0500

    added login password stuff

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_z6lATyESP',
+        userPoolClientId: '4jtt1vl088sldal4lndgjr6b8r',
+        region: 'us-east-1:482949084875'
+    },
+    api: {
+        invokeUrl: 'https://rcrbhb4ldh.execute-api.us-east-1.amazonaws.com/prod',
+    }
+};
